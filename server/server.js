6c07a8e72931e4364dd28791443a8c3c0bed8c29
Marek Tatarko <marek.tatarko@axa-winterthur.ch>
2019-07-22 16:39:53 +0200

    port config 2

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.BA_TEST777_NODEJS_SERVICE_PORT || 8080;
-const server_ip_address = process.env.BA_TEST777_NODEJS_PORT_8080_TCP_ADDR || '127.0.0.1';
+const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 
 app.get('/', (req, res) => res.send('Hello World from simple NodeJS application!'));
